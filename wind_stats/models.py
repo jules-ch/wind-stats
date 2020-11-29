96874c6656af34ba2de1bf470bb6fa7ae1778775
Jules Chéron <jules.cheron@gmail.com>
2020-11-29 22:17:39 +0100

    Update models
    - Get Annual Energy production from site.

--- wind_stats/models.py
+++ wind_stats/models.py
@@ -1,20 +1,36 @@
-from dataclasses import dataclass, field
-from typing import List
+import logging
+from dataclasses import dataclass
+from typing import List, Tuple
 
 import numpy as np
-from numpy.core.fromnumeric import mean
+from pint.quantity import Quantity
 from scipy.integrate import quad
 from scipy.interpolate.interpolate import interp1d
 from scipy.special import gamma
-from scipy.stats import rv_continuous, weibull_min
+from scipy.stats import weibull_min
 
-from wind_stats.constants import AIR_DENSITY
+from .constants import AIR_DENSITY
+from .units import units
 
-from .constants import UREG
+logger = logging.getLogger(__name__)
 
 
-@UREG.wraps("W", ("m**2", "m/s"))
+@units.wraps("W", ("m**2", "m/s"))
 def wind_power(area, wind_speed):
+    """Calculate available wind power
+
+    Parameters
+    ----------
+    area : `pint.Quantity`
+        swept surface
+    wind_speed: `pint.Quantity`
+        wind speed
+
+    Returns
+    -------
+    `pint.Quantity`
+        The available wind power
+    """
     return 0.5 * AIR_DENSITY.m * area * wind_speed ** 3
 
 
@@ -35,15 +51,25 @@ class PowerCurve:
     """
 
     def __init__(self, wind_speed, power) -> None:
-        self.wind_speed = wind_speed * UREG("m/s")
-        self.power = power * UREG.W
+        self.wind_speed = wind_speed * units("m/s")
+        self.power = power * units.W
 
 
 class WindDistribution:
+    """Wind distribution as a Weibull distribution."""
+
     def __init__(self, A, k) -> None:
         self.A = A
         self.k = k
         self.weibull = weibull(A, k)
+
+    @classmethod
+    def from_mean_wind(cls, mean_wind, k):
+        A = mean_wind / gamma(1 + 1 / k)
+        return cls(A, k)
+
+    def power_density(self, wind_speed):
+        return 0.5 * AIR_DENSITY * self.weibull.pdf(wind_speed)
 
 
 @dataclass
@@ -52,18 +78,17 @@ class Site:
     latitude: float
     longitude: float
     distribution: WindDistribution
-    turbines: List["WindTurbine"] = field(default_factory=list)
 
     @property
     def mean_wind(self):
-        return self.distribution.weibull.mean() * UREG("m/s")
+        return self.distribution.weibull.mean() * units("m/s")
 
     @property
     def mean_power_density(self):
         return self.get_mean_power_density()
 
-    @UREG.wraps("W/m**2", None)
-    def get_mean_power_density(self):
+    @units.wraps("W/m**2", None)
+    def get_mean_power_density(self) -> Quantity:
         """Get mean power density in W/m²
 
         .. math::
@@ -77,32 +102,40 @@ class Site:
 
 
 class WindTurbine:
-    def __init__(self, power_curve, diameter) -> None:
-        self.power_curve = power_curve
-        self.diameter = diameter * UREG.m
-        self.site = None
+    def __init__(
+        self,
+        name: str,
+        power_curve: Tuple[List[float], List[float]],
+        diameter: float,
+        height: float,
+    ) -> None:
+        self.name = name
+        self.power_curve = PowerCurve(*power_curve)
+        self.diameter = diameter * units.m
         self.rated_power = max(self.power_curve.power)
-
-    def set_site(self, site: Site):
-        self.site = site
+        self.hub_height = height * units.m
 
     @property
-    def rotor_area(self):
+    def rotor_area(self) -> Quantity:
+        """Swept rotor area."""
         return np.pi * self.diameter ** 2 / 4
 
-    def get_power_coefficients(self):
-        return self.power_curve.power / wind_power(
-            self.rotor_area, self.power_curve.wind_speed
-        )
+    def get_power_coefficients(self) -> Tuple[np.ndarray]:
+        """Get Cp coefficients for wind speeds defined in the power curve."""
 
-    def get_mean_power(self):
+        wind_speeds = self.power_curve.wind_speed
+        available_wind_power = wind_power(self.rotor_area, self.power_curve.wind_speed)
+        cp = self.power_curve.power / available_wind_power
+
+        return wind_speeds.m, cp.m
+
+    def get_mean_power(self, site: Site) -> Quantity:
         """Mean power output
 
         ..math
            \int_{0}^{\infty}(pdf(v) \cdot P(v)) dv
         """
-
-        distribution = self.site.distribution
+        distribution = site.distribution
         wind_speeds = self.power_curve.wind_speed
 
         def f(wind_speed):
@@ -114,9 +147,9 @@ class WindTurbine:
         mean_power, _ = quad(
             f, min(wind_speeds).m, max(wind_speeds).m, points=wind_speeds.m
         )
-        return mean_power * UREG.W
+        return mean_power * units.W
 
-    def get_annual_energy_production(self):
+    def get_annual_energy_production(self, site: Site) -> Quantity:
         """
         Get annual energy production.
 
@@ -125,15 +158,14 @@ class WindTurbine:
         ..math
            \int_{0}^{\infty}(pdf(v) \cdot P(v) \cdot t) dv
 
-        Returns:
+        Returns
+        -------
             Annual energy to be expected in Wh.
         """
 
-        if not self.site:
-            raise ValueError("No site has been set")
-        annual_hours = (1 * UREG.year).to("hours")
+        annual_hours = (1 * units.year).to("hours")
 
-        mean_power = self.get_mean_power()
+        mean_power = self.get_mean_power(site)
         annual_energy_production = annual_hours * mean_power
 
         return annual_energy_production

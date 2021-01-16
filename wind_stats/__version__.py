6ef4cb24d5c162f557178b1d2a0934f688ce7a9c
Jules Chéron <jules.cheron@gmail.com>
2021-01-16 16:01:57 +0100

    Update mypy config & type ignores

--- wind_stats/__version__.py
+++ wind_stats/__version__.py
@@ -1,7 +1,7 @@
-try:  # type: ignore
-    from importlib.metadata import PackageNotFoundError, version
-except ImportError:  # pragma: no cover
-    from importlib_metadata import PackageNotFoundError, version
+try:
+    from importlib.metadata import PackageNotFoundError, version  # type: ignore
+except ImportError:
+    from importlib_metadata import PackageNotFoundError, version  # type: ignore
 
 try:
     __version__ = version(__package__)

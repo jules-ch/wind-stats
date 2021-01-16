fa31b87b82483b1f891978c76d31836fe86db59e
Jules Chéron <jules.cheron@gmail.com>
2021-01-16 15:44:06 +0100

    Retrieve version with importlib.metadata

--- wind_stats/__version__.py
+++ wind_stats/__version__.py
@@ -1,1 +1,9 @@
-__version__ = "0.2.0"
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # Can remove when we require Python > 3.7
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version(__package__)
+except PackageNotFoundError:
+    __version__ = "unknown"
